de2184870c797077ecec3b8fce4be6d13d9568d7
Connie Hyman <constance.hyman@mchsi.com>
2018-11-03 21:07:47 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,12 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{self.first_name} #{self.last_name}"
   end
 
   def list_roles
-    characters.each do |c|
-      "#{c.name} - #{c.show.name}"
+    characters.each do |character|
+      "#{character.name} - #{character.show.name}"
 
     end
   end
